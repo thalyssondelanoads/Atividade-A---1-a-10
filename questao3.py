f55dc6b8123b968340b753e5621743bf62fb4db9
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 20:35:08 -0300

    Questão 3

---
+++ questao3.py
@@ -0,0 +1,7 @@
+print('Transformar Minutos em Horas/Minutos')
+print('------------')
+mn =int(input('Qual valor em Minutos?: '))
+print('--------------')
+resultado_horas = int(mn / 60)
+resultado_min = mn % 60
+print(f'Resultado = {resultado_horas} Horas e {resultado_min} Minutos!')
