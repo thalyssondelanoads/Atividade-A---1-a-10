8238565427d4bde0dbbebdf8f9b8ed5a61ebbbe8
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 17:21:35 -0300

    questao2.py
    Questão 2

---
+++ questao2.py
@@ -0,0 +1,8 @@
+print (' Conversor de Horas e Minutos ')
+print (' ------------ ')
+hr = float(input(' Valor em Horas : '))
+mn = float(input(' Valor em Minutos: '))
+print ('---------------')
+mn2 = hr * 60
+resultado = mn2 + mn
+print (f' O resultado é {resultado} Minutos ! ')
