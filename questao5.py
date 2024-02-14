d25b952c386879908e9546c478bc769f04604176
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 21:00:16 -0300

    Questão 5

---
+++ questao5.py
@@ -0,0 +1,10 @@
+print('Soma de elementos de um número inteiro')
+print('-----------')
+print('O número precisa ter obrigatoriamente 3 dígitos')
+print('------------')
+numero = int(input(' Digite o número: '))
+alg_1 = int(numero / 100)
+alg_2 = int(numero % 100 / 10)
+alg_3 = int(numero % 10 )
+resultado = alg_1 + alg_2 + alg_3 
+print(f' Resultado = {resultado}')
