b7187405cbb3aaf5e69f668592452d610db808e7
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 21:19:37 -0300

    Questão 8

---
+++ questao8.py
@@ -0,0 +1,7 @@
+print('Divisão da soma pela subtração de um Número')
+print('-----------')
+numA = int(input('Digite um Número: '))
+numB = int(input('Digite outro Número: '))
+resultado = (numA + numB) / (numA - numB )
+print('------------')
+print(f'A divisão da soma pela subtração dos algarismos é {resultado} !')
