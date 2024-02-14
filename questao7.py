9e5478276406a7f40d4315dd1047512d3bce7bc6
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 21:06:04 -0300

    Questão 7

---
+++ questao7.py
@@ -0,0 +1,10 @@
+print (' Operações com Inteiros |A,B,C|')
+print (' ------------ ')
+A = int(input(' Número A: '))
+B = int(input(' Número B: '))
+C = int(input(' Número C: '))
+print('-------------- ')
+soma_ab = A + B
+dif_bc = B - C
+print (f' A soma entre A e B = [{soma_ab}] ')
+print (f' A diferença entre B e C = [{dif_bc}] ')
