58d51add352e3ef4012becc755779eff7e53e514
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-13 21:32:08 -0300

    Questão 9

---
+++ questao9.py
@@ -0,0 +1,8 @@
+print('Inverter Números ')
+print('----------')
+A = int(input('Digite o 1° Número: '))
+B = int(input('Digite o 2° Número: '))
+print('-----------')
+print(f'Seus Números são ({A},{B})')
+print('---------')
+print(f'A inversão deles é ({B},{A})')
