5122ebfa9eaa78bb084deddb9d5b241f2342356d
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:10:47 -0300

    Atualizar o questao5.py

--- questao5.py
+++ questao5.py
@@ -1,8 +1,12 @@
 print('Soma de elementos de um número inteiro')
 print('-----------')
+
 numero = int(input(' Digite o número: '))
+
 alg_1 = int(numero / 100)
 alg_2 = int(numero % 100 / 10)
 alg_3 = int(numero % 10 )
+
+print('------------')
 resultado = alg_1 + alg_2 + alg_3 
 print(f' Resultado = {resultado}')
