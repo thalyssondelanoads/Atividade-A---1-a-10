a8d0e4d7f3d19acad241d05349eb4ff4ff76ce0c
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:14:31 -0300

    Atualizar o questao9.py

--- questao9.py
+++ questao9.py
@@ -1,7 +1,9 @@
 print('Inverter Números ')
 print('----------')
+
 A = int(input('Digite o 1° Número: '))
 B = int(input('Digite o 2° Número: '))
+
 print('-----------')
 print(f'Seus Números são ({A},{B})')
 print('---------')
