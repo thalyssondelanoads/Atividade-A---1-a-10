16d7468033b1330f403828f7ddd400cf3eee40c5
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:09:09 -0300

    Atualizar o questao4.py

--- questao4.py
+++ questao4.py
@@ -2,7 +2,7 @@ print ('Conversor de Dólar para Real')
 print ('Cotação do Dólar ≈ 5 Reais ')
 print (' ---------------- ')
 
-dolar =float(input('Qual valor deseja converter?'))
+dolar = float(input(' Qual valor deseja converter? '))
 
 conversao = dolar * 5
 
