42faebfe8c44aa43e048cd120051b572be2216d0
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:16:58 -0300

    Atualizar o questao_10.py

--- questao_10.py
+++ questao_10.py
@@ -1,9 +1,12 @@
 print('Divisão e Resto de um número inteiro ')
 print('--------')
+
 num1 = int(input('Digite o 1° Número: '))
 num2 = int(input('Digite o 2° Número: '))
+
 quociente = int(num1 / num2)
 resto = (num1 % num2)
+
 print('---------')
 print(f'A divisão do 1° Número pelo 2° é {quociente}')
 print('____')
