a50094270e4999050a55f0a881982f9f779e0a8a
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-16 16:08:06 -0300

    Atualizar o questao3.py

--- questao3.py
+++ questao3.py
@@ -5,6 +5,6 @@ mn =int(input('Qual valor em Minutos?: '))
 print('--------------')
 
 resultado_horas = int(mn / 60)
-
 resultado_min = mn % 60
+
 print(f'Resultado = {resultado_horas} Horas e {resultado_min} Minutos!')
